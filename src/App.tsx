cc8f545f51f31ac9ab4d3f1c4322c8702ac4aeed
Berke Kalkan <berkekalkantr@gmail.com>
2022-03-09 20:42:10 +0300

    Create basic login structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Component, Fragment } from 'react';
+import {   
+  BrowserRouter,
+  Routes,
+  Route,
+  Link, } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuthService from "./services/auth.service";
+import IUser from './types/user.type';
+
+import Home from "./components/home.component";
+import Login from "./components/login.component";
+
+import EventBus from "./common/EventBus";
+
+type Props = any;
+
+type State = {
+  currentUser: string | undefined
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+      });
+    }
+
+    EventBus.on("logout", this.logOut);
+  }
+
+  componentWillUnmount() {
+    EventBus.remove("logout", this.logOut);
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      currentUser: undefined,
+    });
+  }
+
+  render() {
+    const currentUser = this.state;
+
+    return (
+      <div>
+        <header className="p-3 bg-dark text-white">
+          <div className="container">
+            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+              </a>
+      
+              <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <Link to={"/"} className="nav-link px-2 text-secondary">Home</Link>
+              </ul>
+      
+              <div className="text-end">
+              {currentUser ? (
+                <Fragment>
+                  <Link to={"/dashboard"} className="btn btn-outline-light me-2">Dashboard</Link>
+                  <Link to={"/login"} className="btn btn-warning" onClick={this.logOut}>Logout</Link>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <Link to={"/login"} className="btn btn-outline-light me-2">Login</Link>
+                  <Link to={"/signup"} className="btn btn-warning">Sign Up</Link>
+                </Fragment>
+              )}
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/login.component.tsx
@@ -0,0 +1,115 @@
+import { Component } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
+import AuthService from "../services/auth.service";
+
+type Props = any;
+
+type State = {
+  email: string,
+  password: string,
+  loading: boolean,
+  message: string
+};
+
+export default class Login extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      message: ""
+    };
+  }
+
+  handleLogin(formValue: { email: string; password: string }) {
+    const { email, password } = formValue;
+
+    this.setState({
+      message: "",
+      loading: true
+    });
+
+
+    AuthService.login(email, password).then(
+      () => {
+        window.location.reload();
+      },
+      error => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        this.setState({
+          loading: false,
+          message: resMessage
+        });
+      }
+    );
+  }
+
+  render() {
+    const { loading, message } = this.state;
+
+    const initialValues = {
+      email: "",
+      password: "",
+    };
+
+    return (
+      <div className="col-md-12">
+        <div className="card card-container">
+          <Formik
+            initialValues={initialValues}
+            onSubmit={this.handleLogin}
+          >
+            <Form>
+              <div className="form-group">
+                <label htmlFor="email">email</label>
+                <Field name="email" type="text" className="form-control" />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="alert alert-danger"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Field name="password" type="password" className="form-control" />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="alert alert-danger"
+                />
+              </div>
+
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary btn-block" disabled={loading}>
+                  {loading && (
+                    <span className="spinner-border spinner-border-sm"></span>
+                  )}
+                  <span>Login</span>
+                </button>
+              </div>
+
+              {message && (
+                <div className="form-group">
+                  <div className="alert alert-danger" role="alert">
+                    {message}
+                  </div>
+                </div>
+              )}
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    );
+  }
+}
